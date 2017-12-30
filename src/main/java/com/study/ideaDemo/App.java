589b1b87c2e6959cd353e2fac77b65083bbe3235
mm <13826005211@163.com>
2017-12-30 23:17:50 +0800

    create project mm java

---
+++ src/main/java/com/study/ideaDemo/App.java
@@ -0,0 +1,13 @@
+package com.study.ideaDemo;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
